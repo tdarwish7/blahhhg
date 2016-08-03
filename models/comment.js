4764426a0dbd2d648eafb58d8b4bde9ce7d12ffc
Teacher <Teacher>
2016-08-03 18:34:14 -0400

    created basic comment model

---
+++ models/comment.js
@@ -0,0 +1,31 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: 'User'
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    required: true
+  },
+  updated: {
+    type: Date,
+    required: true
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Post'
+  }
+});
+
+var Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;

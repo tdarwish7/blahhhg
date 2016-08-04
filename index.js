eacc8e0c3b9025d019316ae97682fd84d3a9c31b
Teacher <Teacher>
2016-08-03 20:13:01 -0400

    createrd posts router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var server = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var commentRouter = require('./routes/comments.js');
+var postRouter = require('./routes/posts.js');
 
 
 var port = process.env.PORT || 8080;
@@ -12,6 +13,7 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 server.use(commentRouter);
+server.use(postRouter);
 
 server.get('/', function(req, res){
   res.send('hello!');
--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,36 @@
 
 var express = require('express');
 var router = express.Router();
+
+router.get('/posts', getPosts);
+router.get('/posts/:postId', getSpecificPosts);
+router.post('/posts', createPosts);
+router.put('/posts/:postId', updatePost);
+router.delete('/posts/:postsId', deletePost);
+
+module.exports = router;
+
+function getPosts(req, res, next){
+  console.log('getting all the posts');
+  next();
+}
+
+function getSpecificPosts(req, res, next){
+  console.log('get a particular post');
+  next();
+}
+
+function createPosts(req, res, next){
+  console.log('creating a Post');
+  next();
+}
+
+function updatePost(req, res, next){
+  console.log('updating a Post');
+  next();
+}
+
+function deletePost(req, res, next){
+  console.log('delete a Post');
+  next();
+}
